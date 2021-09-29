6c16c9c252fd13d0d30b9a902a3951bfba609a6b
Lu <gonzalezmontalvolucero@gmail.com>
2021-09-29 09:52:32 -0500

    Cambio titulo

--- template/head.php
+++ template/head.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
-    <title>Practica 1</title>
+    <title>Practica DevOps</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css?h=b355b6f3fe3df60c9b28bfd8b17296d8" />
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" />
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.0/css/all.css" />
